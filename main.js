58022c894a2ba31cabe550f409036ed4fd5c159a
bogdy234 <filimonbogdan89@gmail.com>
2022-11-03 12:09:44 +0200

    remove first page animation

--- main.js
+++ main.js
@@ -140,28 +140,3 @@ menuButton.addEventListener("click", () => {
         dropdown.style = "display: block";
     }
 });
-
-const anchor = document.querySelector("#anchor");
-const rect = anchor.getBoundingClientRect();
-const anchorX = rect.left + rect.width / 2;
-const anchorY = rect.top + rect.height / 2;
-const eyes = document.querySelectorAll(".eye");
-
-function angle(cx, cy, ex, ey) {
-    const dy = ey - cy;
-    const dx = ex - cx;
-    const rad = Math.atan2(dy, dx);
-    const deg = (rad * 180) / Math.PI;
-    return deg;
-}
-
-document.addEventListener("mousemove", (e) => {
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
-
-    const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
-
-    eyes.forEach((eye) => {
-        eye.style.transform = `rotate(${-60 + angleDeg}deg)`;
-    });
-});
